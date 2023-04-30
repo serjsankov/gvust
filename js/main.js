098db7ac87715149a0321d251b2301d19cd8ac7b
serjsankov <serj.sankov@gmail.com>
2023-04-30 03:36:09 +0300

    add new page

---
+++ js/main.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector(".header");
+  const containerSelectBrans = document.querySelector(".js-container-select-brands")
+  initClicks();
+  initCustomSelect();
+  function initClicks() {
+    document.addEventListener("click", (event) => {
+      if (event.target.closest(".burger")) {
+        header.classList.toggle("active");
+        if (header.classList.contains("active")) {
+          document.body.style.overflow = "hidden";
+        } else {
+          document.body.style.overflow = "scroll";
+        }
+      } if (event.target.closest(".js-show-select-brand")) { 
+        event.preventDefault();
+        containerSelectBrans.classList.toggle("active");
+      }
+    });
+  };
+  function initCustomSelect() { 
+    $("select").selectize();
+  };
+});
